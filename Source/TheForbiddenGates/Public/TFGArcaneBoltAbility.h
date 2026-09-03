01e00f18f05874eb3477728541b12989878e5bdf
chaukentsakiso55-a11y <chaukentsakiso55@gmail.com>
2026-09-03 19:51:49 +0200

    Add Arcane Bolt starter spell class

---
+++ Source/TheForbiddenGates/Public/TFGArcaneBoltAbility.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TFGMagicProjectileAbility.h"
+#include "TFGArcaneBoltAbility.generated.h"
+
+UCLASS(Blueprintable)
+class THEFORBIDDENGATES_API UTFGArcaneBoltAbility : public UTFGMagicProjectileAbility
+{
+    GENERATED_BODY()
+
+public:
+    UTFGArcaneBoltAbility();
+};
