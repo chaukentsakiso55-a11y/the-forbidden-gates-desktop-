f3a1c1c3294fa48117b93c6de352a2622abcac26
chaukentsakiso55-a11y <chaukentsakiso55@gmail.com>
2026-09-03 19:51:05 +0200

    Add instant magic damage gameplay effect

---
+++ Source/TheForbiddenGates/Public/TFGDamageGameplayEffect.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "TFGDamageGameplayEffect.generated.h"
+
+UCLASS()
+class THEFORBIDDENGATES_API UTFGDamageGameplayEffect : public UGameplayEffect
+{
+    GENERATED_BODY()
+
+public:
+    UTFGDamageGameplayEffect();
+};
