aa7b63c46c79657d042a48171f71f6ca7d6c390f
chaukentsakiso55-a11y <chaukentsakiso55@gmail.com>
2026-09-05 20:03:01 +0200

    Expose production content profiles to Blueprints

--- Source/TheForbiddenGates/Public/TFGLevelContentProfile.h
+++ Source/TheForbiddenGates/Public/TFGLevelContentProfile.h
@@ -1,24 +1,28 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "TFGLevelContentProfile.generated.h"
 
+USTRUCT(BlueprintType)
 struct FTFGLevelContentProfile
 {
-    int32 LevelNumber = 1;
-    int32 ChapterNumber = 1;
-    FName EnvironmentTheme = NAME_None;
-    FName LightingMood = NAME_None;
-    FName WeatherState = NAME_None;
-    FName MusicState = NAME_None;
-    FName IntroCinematicId = NAME_None;
-    FName OutroCinematicId = NAME_None;
-    FName TraversalMechanic = NAME_None;
-    FName PuzzleArchetype = NAME_None;
-    FName BossId = NAME_None;
-    FString ArtFolder;
-    FString AudioFolder;
-    FString VFXFolder;
-    bool bHeroLevel = false;
+    GENERATED_BODY()
+
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") int32 LevelNumber = 1;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") int32 ChapterNumber = 1;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") FName EnvironmentTheme = NAME_None;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") FName LightingMood = NAME_None;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") FName WeatherState = NAME_None;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") FName MusicState = NAME_None;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") FName IntroCinematicId = NAME_None;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") FName OutroCinematicId = NAME_None;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") FName TraversalMechanic = NAME_None;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") FName PuzzleArchetype = NAME_None;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") FName BossId = NAME_None;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") FString ArtFolder;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") FString AudioFolder;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") FString VFXFolder;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Production") bool bHeroLevel = false;
 };
 
 class THEFORBIDDENGATES_API FTFGLevelContentCatalog
