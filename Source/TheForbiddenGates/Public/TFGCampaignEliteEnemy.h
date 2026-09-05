793e7d390e0bb4c6d7d7c86def67217d21e0e885
chaukentsakiso55-a11y <chaukentsakiso55@gmail.com>
2026-09-05 20:01:03 +0200

    Upgrade elite enemies into milestone bosses

--- Source/TheForbiddenGates/Public/TFGCampaignEliteEnemy.h
+++ Source/TheForbiddenGates/Public/TFGCampaignEliteEnemy.h
@@ -4,6 +4,8 @@
 #include "TFGEnemyCharacter.h"
 #include "TFGCampaignEliteEnemy.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FTFGBossPhaseChanged, int32, NewPhase, FName, BossId);
+
 UCLASS()
 class THEFORBIDDENGATES_API ATFGCampaignEliteEnemy : public ATFGEnemyCharacter
 {
@@ -11,4 +13,18 @@ class THEFORBIDDENGATES_API ATFGCampaignEliteEnemy : public ATFGEnemyCharacter
 
 public:
     ATFGCampaignEliteEnemy();
+    virtual void Tick(float DeltaSeconds) override;
+
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Boss") FName BossId = NAME_None;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Boss") FText BossDisplayName;
+    UPROPERTY(BlueprintReadOnly, Category="Forbidden Gates|Boss") int32 CurrentPhase = 1;
+    UPROPERTY(BlueprintAssignable, Category="Forbidden Gates|Boss") FTFGBossPhaseChanged OnBossPhaseChanged;
+
+protected:
+    virtual void BeginPlay() override;
+
+private:
+    void ConfigureForCurrentLevel();
+    void UpdateBossPhase();
+    bool bNamedBoss = false;
 };
