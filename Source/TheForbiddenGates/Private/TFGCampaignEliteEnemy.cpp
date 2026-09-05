41dffd9e1f7a1b409d607fd8628656118f380fb6
chaukentsakiso55-a11y <chaukentsakiso55@gmail.com>
2026-09-05 19:31:17 +0200

    Implement campaign elite enemy stats

---
+++ Source/TheForbiddenGates/Private/TFGCampaignEliteEnemy.cpp
@@ -0,0 +1,12 @@
+#include "TFGCampaignEliteEnemy.h"
+
+ATFGCampaignEliteEnemy::ATFGCampaignEliteEnemy()
+{
+    EnemyId = TEXT("CampaignElite");
+    StartingHealth = 180.0f;
+    StartingMana = 80.0f;
+    StartingStamina = 120.0f;
+    StartingMagicPower = 22.0f;
+    StartingArmor = 14.0f;
+    ExperienceReward = 45;
+}
